cec7df9411ae11dd6e62b8d4ccff2bdd2a468502
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-04 10:26:51 -0700

    Add model test for display_name class method

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,22 @@ describe Actor do
     it { should have_many :actor_movies}
     it { should have_many(:movies).through(:actor_movies)}
   end
+  describe 'class methods' do
+    it 'display_name for actors youngest to oldest' do
+
+      studio_1 = Studio.create!(name: 'Fabulous Studio', location: 'Colorado')
+
+      movie_1 = studio_1.movies.create!(title: 'Hunger Games', creation_year: 2017, genre: 'Fiction')
+
+      actor_1 = Actor.create!(name: 'Leonardo di Caprio', age: 45)
+      actor_2 = Actor.create!(name: 'Tom Cruise', age: 55)
+      actor_3 = Actor.create!(name: 'Brad Pitt', age: 50)
+
+      ActorMovie.create!(actor_id: actor_1.id, movie_id: movie_1.id)
+      ActorMovie.create!(actor_id: actor_2.id, movie_id: movie_1.id)
+      ActorMovie.create!(actor_id: actor_3.id, movie_id: movie_1.id)
+
+      expect(movie_1.actors.display_name).to eq([actor_1.name, actor_3.name, actor_2.name])
+    end
+  end
 end
