7d38b2b1cd46cf8b35e57e1e4766737d0b1aa316
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-04 10:46:14 -0700

    Add model test for average age class method for actors

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -22,5 +22,21 @@ describe Actor do
 
       expect(movie_1.actors.order_by_age).to eq([actor_1, actor_3, actor_2])
     end
+    it 'average_age' do
+
+      studio_1 = Studio.create!(name: 'Fabulous Studio', location: 'Colorado')
+
+      movie_1 = studio_1.movies.create!(title: 'Hunger Games', creation_year: 2017, genre: 'Fiction')
+
+      actor_1 = Actor.create!(name: 'Leonardo di Caprio', age: 45)
+      actor_2 = Actor.create!(name: 'Tom Cruise', age: 55)
+      actor_3 = Actor.create!(name: 'Brad Pitt', age: 50)
+
+      ActorMovie.create!(actor_id: actor_1.id, movie_id: movie_1.id)
+      ActorMovie.create!(actor_id: actor_2.id, movie_id: movie_1.id)
+      ActorMovie.create!(actor_id: actor_3.id, movie_id: movie_1.id)
+
+      expect(movie_1.actors.average_age).to eq(50)
+    end
   end
 end
