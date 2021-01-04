7e31de4a841dc0283e5ce0cf6e70945bfc75117d
Vanessa Arruda <wanessaarruda@gmail.com>
2021-01-04 10:21:29 -0700

    Add rspec for actor by age

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -21,5 +21,24 @@ describe Movie do
       expect(page).to have_content(movie_1.creation_year)
       expect(page).to have_content(movie_1.genre)
     end
+    it 'Shows all actors from youngest to oldest' do
+
+      studio_1 = Studio.create!(name: 'Fabulous Studio', location: 'Colorado')
+
+      movie_1 = studio_1.movies.create!(title: 'Hunger Games', creation_year: 2017, genre: 'Fiction')
+
+      actor_1 = Actor.create!(name: 'Leonardo di Caprio', age: 45)
+      actor_2 = Actor.create!(name: 'Tom Cruise', age: 55)
+      actor_3 = Actor.create!(name: 'Brad Pitt', age: 50)
+
+      ActorMovie.create!(actor_id: actor_1.id, movie_id: movie_1.id)
+      ActorMovie.create!(actor_id: actor_2.id, movie_id: movie_1.id)
+      ActorMovie.create!(actor_id: actor_3.id, movie_id: movie_1.id)
+
+      visit movie_path(movie_1)
+
+      expect(actor_1.name).to appear_before(actor_3.name)
+      expect(actor_3.name).to appear_before(actor_2.name)
+    end
   end
 end
